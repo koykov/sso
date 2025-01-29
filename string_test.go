857ede54a356bfb64434eef9ec4587bb9e2bc6a5
Konstantin Voykov <koykov0@gmail.com>
2025-01-30 00:10:34 +0300

    testing: naming issues

--- string_test.go
+++ string_test.go
@@ -3,33 +3,33 @@ package sso
 import "testing"
 
 func TestSSO(t *testing.T) {
-	t.Run("assign/short", func(t *testing.T) {
+	t.Run("assign/sso", func(t *testing.T) {
 		s := New("foobar")
 		if s.String() != "foobar" {
 			t.Fail()
 		}
 	})
-	t.Run("assign/long", func(t *testing.T) {
+	t.Run("assign/heap", func(t *testing.T) {
 		s := New("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
 		if s.String() != "Lorem ipsum dolor sit amet, consectetur adipiscing elit." {
 			t.Fail()
 		}
 	})
-	t.Run("append/short+short", func(t *testing.T) {
+	t.Run("append/sso", func(t *testing.T) {
 		s := New("hello")
 		s.AppendString(" world!")
 		if s.String() != "hello world!" {
 			t.Fail()
 		}
 	})
-	t.Run("append/short+long", func(t *testing.T) {
+	t.Run("append/sso_to_heap", func(t *testing.T) {
 		s := New("Lorem ipsum")
 		s.AppendString(" dolor sit amet, consectetur adipiscing elit.")
 		if s.String() != "Lorem ipsum dolor sit amet, consectetur adipiscing elit." {
 			t.Fail()
 		}
 	})
-	t.Run("append/long+long", func(t *testing.T) {
+	t.Run("append/heap", func(t *testing.T) {
 		s := New("Lorem ipsum dolor sit amet,")
 		s.AppendString(" consectetur adipiscing elit.")
 		if s.String() != "Lorem ipsum dolor sit amet, consectetur adipiscing elit." {
@@ -39,35 +39,35 @@ func TestSSO(t *testing.T) {
 }
 
 func BenchmarkSSO(b *testing.B) {
-	b.Run("assign/short", func(b *testing.B) {
+	b.Run("assign/sso", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("foobar")
 			_ = s
 		}
 	})
-	b.Run("assign/long", func(b *testing.B) {
+	b.Run("assign/heap", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
 			_ = s
 		}
 	})
-	b.Run("append/short+short", func(b *testing.B) {
+	b.Run("append/sso", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("hello")
 			s.AppendString(" world!")
 		}
 	})
-	b.Run("append/short+long", func(b *testing.B) {
+	b.Run("append/sso_to_heap", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("Lorem ipsum")
 			s.AppendString(" dolor sit amet, consectetur adipiscing elit.")
 		}
 	})
-	b.Run("append/long+long", func(b *testing.B) {
+	b.Run("append/heap", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("Lorem ipsum dolor sit amet,")
