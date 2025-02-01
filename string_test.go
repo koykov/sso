6d93942579ca211b2ccf03a02cf0f93573ccc92d
Konstantin Voykov <koykov0@gmail.com>
2025-02-01 22:57:32 +0300

    comparison bench; naming issues

--- string_test.go
+++ string_test.go
@@ -3,35 +3,35 @@ package sso
 import "testing"
 
 func TestSSO(t *testing.T) {
-	t.Run("assign/sso", func(t *testing.T) {
+	t.Run("copy/sso", func(t *testing.T) {
 		s := New("foobar")
 		if s.String() != "foobar" {
 			t.Fail()
 		}
 	})
-	t.Run("assign/heap", func(t *testing.T) {
+	t.Run("copy/heap", func(t *testing.T) {
 		s := New("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
 		if s.String() != "Lorem ipsum dolor sit amet, consectetur adipiscing elit." {
 			t.Fail()
 		}
 	})
-	t.Run("append/sso", func(t *testing.T) {
+	t.Run("concat/sso", func(t *testing.T) {
 		s := New("hello")
-		s.AppendString(" world!")
+		s.Concat(" world!")
 		if s.String() != "hello world!" {
 			t.Fail()
 		}
 	})
-	t.Run("append/sso_to_heap", func(t *testing.T) {
+	t.Run("concat/sso_to_heap", func(t *testing.T) {
 		s := New("Lorem ipsum")
-		s.AppendString(" dolor sit amet, consectetur adipiscing elit.")
+		s.Concat(" dolor sit amet, consectetur adipiscing elit.")
 		if s.String() != "Lorem ipsum dolor sit amet, consectetur adipiscing elit." {
 			t.Fail()
 		}
 	})
-	t.Run("append/heap", func(t *testing.T) {
+	t.Run("concat/heap", func(t *testing.T) {
 		s := New("Lorem ipsum dolor sit amet,")
-		s.AppendString(" consectetur adipiscing elit.")
+		s.Concat(" consectetur adipiscing elit.")
 		if s.String() != "Lorem ipsum dolor sit amet, consectetur adipiscing elit." {
 			t.Fail()
 		}
@@ -39,39 +39,64 @@ func TestSSO(t *testing.T) {
 }
 
 func BenchmarkSSO(b *testing.B) {
-	b.Run("assign/sso", func(b *testing.B) {
+	b.Run("copy/sso", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("foobar")
 			_ = s
 		}
 	})
-	b.Run("assign/heap", func(b *testing.B) {
+	b.Run("copy/heap", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
 			_ = s
 		}
 	})
-	b.Run("append/sso", func(b *testing.B) {
+	b.Run("concat/sso", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("hello")
-			s.AppendString(" world!")
+			s.Concat(" world!")
 		}
 	})
-	b.Run("append/sso_to_heap", func(b *testing.B) {
+	b.Run("concat/sso_to_heap", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("Lorem ipsum")
-			s.AppendString(" dolor sit amet, consectetur adipiscing elit.")
+			s.Concat(" dolor sit amet, consectetur adipiscing elit.")
 		}
 	})
-	b.Run("append/heap", func(b *testing.B) {
+	b.Run("concat/heap", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			s := New("Lorem ipsum dolor sit amet,")
-			s.AppendString(" consectetur adipiscing elit.")
+			s.Concat(" consectetur adipiscing elit.")
+		}
+	})
+}
+
+func BenchmarkString(b *testing.B) {
+	b.Run("copy", func(b *testing.B) {
+		b.ReportAllocs()
+		x := "foobar"
+		for i := 0; i < b.N; i++ {
+			s := x + ""
+			_ = s
+		}
+	})
+	b.Run("concat", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			s := "hello"
+			s = s + " world!"
+		}
+	})
+	b.Run("concat", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			s := "Lorem ipsum dolor sit amet,"
+			s = s + " consectetur adipiscing elit."
 		}
 	})
 }
